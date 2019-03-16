09b3110681e27d292f16d44c157700597ee18847
QingShanLuoYue <980575067@qq.com>
2019-03-16 14:40:02 +0800

    fix: 添加observe条件判断，不是所有数据都可以被observe的哦

--- Observer.js
+++ Observer.js
@@ -13,6 +13,10 @@ obj.x = 1
 
 // 遍历对象，给每个属性添加getter、setter
 function observe(obj) {
+  if (!obj || typeof obj !== 'object') {  // 不存在或者不是个对象，则返回；如果传进来一个字符串，Object.keys会将字符串解析成单个字符，进入后续步骤造成栈溢出
+    return
+  }
+  // debugger 可以在这里尝试debugger，会发现以上栈溢出
   Object.keys(obj).forEach(key => {
     defineProperty(obj, key, obj[key])
   })
