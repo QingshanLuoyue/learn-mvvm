b95e45bfcc3859b2003c001442b4a94024e33e07
QingShanLuoYue <980575067@qq.com>
2019-03-17 10:40:52 +0800

    在设置getter、setter，进行数据劫持的时候，添加消息订阅器

---
+++ step03/Observer.js
@@ -0,0 +1,58 @@
+// 劫持数据，监听数据变化，通知订阅者，执行绑定的回调函数
+let obj = {
+  x: 'test'
+}
+observe(obj)
+obj.x
+// get test
+// test
+
+obj.x = 1
+// set 1
+// 1
+
+// 深度遍历对象，给每个属性添加getter、setter
+function observe(obj) {
+  if (!obj || typeof obj !== 'object') {
+    // 不存在或者不是个对象，则返回；如果传进来一个字符串，Object.keys会将字符串解析成单个字符，进入后续步骤造成栈溢出
+    return
+  }
+  // debugger // 可以在这里尝试debugger，会发现如果注释以上if条件，将会栈溢出
+  Object.keys(obj).forEach(key => {
+    defineProperty(obj, key, obj[key])
+  })
+}
+// 设置getter、setter
+function defineProperty(obj, key, val) {
+  let dep = new Dep()
+  observe(val) // 监听子属性
+
+  Object.defineProperty(obj, key, {
+    enumerable: true,
+    configurable: false,
+    get() {
+      console.log('get :', val)
+      return val
+    },
+    set(newVal) {
+      console.log('set :', newVal)
+      val = newVal
+      dep.notify()
+    }
+  })
+}
+
+// 消息订阅器
+class Dep {
+  constructor() {
+    this.subs = []
+  }
+  addSub(sub) {
+    this.subs.push(sub)
+  }
+  notify() {
+    this.subs.forEach(sub => {
+      sub.update()
+    })
+  }
+}
